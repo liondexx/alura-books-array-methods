1eeb1c7b15184a3992e15cddb1f4cdd6180ea090
lion patrocinio <lionfilesoffices@gmail.com>
2022-09-09 22:25:31 -0300

    Displaying elements from array para o HTML, and add fuction exibirLivrosNaTela

--- app/main.js
+++ app/main.js
@@ -1,9 +1,31 @@
 let livros = [];
 const endPointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaAPI()
+const elementoParaInserirLivors = document.getElementById('livros');
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endPointDaAPI);
     livros = await res.json();
     console.table(livros);
+    exibirOsLivosNaTela(livros);
 }
+
+function exibirOsLivosNaTela(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivors.innerHTML += `
+        <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}"/>
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">${livro.preco}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+        </div>
+        `
+    });
+
+}
+
